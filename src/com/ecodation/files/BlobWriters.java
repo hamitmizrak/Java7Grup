b771bd8adb69f1edcbed396b4c546ac52a32ba0c
HamitMizrak <hamitmizrak@gmail.com>
2021-03-20 17:58:49 +0300

    düzeltme

---
+++ src/com/ecodation/files/BlobWriters.java
@@ -0,0 +1,24 @@
+package com.ecodation.files;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BlobWriters {
+
+	public static void main(String[] args) {
+
+		try {
+			String databaseGelenVeri = "a{\r\n" + "	color:red;\r\n" + "	background-color:black;\r\n" + "	\r\n"
+					+ "}\r\n" + "*{\r\n" + "	padding:0;\r\n" + "	margin:0;\r\n" + "	box-sizing:border-box;\r\n"
+					+ "}\r\n" + "\r\n" + ".f20{\r\n" + "	font-size:20rem;\r\n" + "}\r\n";
+
+			Files.write(Paths.get("C:\\fileIO\\deneme2.css"), databaseGelenVeri.getBytes(Charset.forName("utf-8")));
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+}
